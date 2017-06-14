d00a1f45c307d0ac2dbdfa6a82b0cde344cc6c62
apoorvumang <apoorvumang@gmail.com>
2017-06-14 16:58:09 +0530

    fixed mongodb uri

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : process.env.MONGOLAB_URI || 'mongodb://localhost/dranurag' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : process.env.MONGODB_URI || 'mongodb://localhost/dranurag' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
