49247f7591a8d1c050789386a0b3745a99ef0299
arpit <arpit.tarang@gmail.com>
2017-09-11 21:28:12 +0530

    added bmi calculation

--- routes/addPatient.js
+++ routes/addPatient.js
@@ -6,6 +6,16 @@ function toTitleCase(str)
     return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 }
 
+function bmi(weightKg, heightCm) {
+  if (!weightKg || !heightCm)
+    return null
+  weightKg = parseInt(weightKg)
+  heightCm = parseInt(heightCm)
+  ret = parseFloat(weightKg / ((heightCm / 100) * (heightCm / 100))).toFixed(2)
+  console.log("bmi is " + ret)
+  return ret
+}
+
 module.exports = (app) => {
   // add patient page
   app.get('/addPatient', (req, res) => {
@@ -17,6 +27,7 @@ module.exports = (app) => {
     // dd/mm/yyyy conversion, now handled client side only
     // req.body.dateOfBirth =  req.body.dateOfBirth.replace( /(\d{2})\/(\d{2})\/(\d{4})/, "$3-$2-$1")
     req.body.name = toTitleCase(req.body.name)
+    req.body.bmi = bmi(req.body.weight, req.body.height)
     console.log(req.body)
     new Patient(req.body)
       .save()
