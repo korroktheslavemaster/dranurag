403b7abed1e740804ef82cd765c577e3dd055b5e
arpit <arpit.tarang@gmail.com>
2017-10-22 21:13:52 +0530

    fixed fix-age.js script, need to run it again

--- scripts/fix-age.js
+++ scripts/fix-age.js
@@ -6,13 +6,15 @@ const patients = db.get("patients");
 
 patients.find().then(docs => {
   docs.forEach(doc => {
-    if (doc.age) {
-      doc.inferredBirthdate = moment()
-        .subtract(doc.age, "years")
-        .toDate();
+    if (doc.age || doc.age == undefined) {
+      if (doc.age)
+        doc.inferredBirthdate = moment()
+          .subtract(doc.age, "years")
+          .toDate();
+      delete doc.age;
       patients
         .findOneAndUpdate({ _id: doc._id }, doc)
-        .then(elm => console.log(elm));
+        .then(elm => console.log(elm._id));
     }
   });
 });
